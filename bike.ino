ae7ad4c558178a85b7cfa43130105eee9ef8b0fb
Cameron Taylor <cam@rtastudio.com>
2018-04-11 16:46:23 -0400

    Added Serial debugging and bug fixes
    For some reason, adding print statements fixed the timing issue where the turn signal was not flashing on schedule.
    
    Also fixed a bug where the turn signals didn't work because time = millis() was in the wrong spot.

--- bike.ino
+++ bike.ino
@@ -6,33 +6,78 @@
 #define PIN 7
 #define LEDS 300
 
+// Setup for the LED strip
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS, PIN, NEO_GRB + NEO_KHZ800);
 uint32_t off = strip.Color(0, 0, 0);
 uint32_t red = strip.Color(255, 0, 0);
 uint32_t orange = strip.Color(128, 128, 0);
 
 int left=0, right=0, brake=0;
-long time = 0;
-int debounce = 200;
+long time = 0, flash = 0, debounce = 200;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600); // This was originally for debugging, but leaving it in had fixed a timing issue
   strip.begin();
   strip.show();
-  pinMode(LEFT, INPUT);
+  pinMode(LEFT, INPUT); // setup pins as inputs
   pinMode(RIGHT, INPUT);
   pinMode(BRAKE, INPUT);
-  digitalWrite(LEFT, HIGH);
+  digitalWrite(LEFT, HIGH); // enable pull up resistors
   digitalWrite(RIGHT, HIGH);
   digitalWrite(BRAKE, HIGH);
 }
 
+void left_turn(){
+  if(left){
+    // flash turn signal
+    if(flash % 500 < 250){
+      Serial.print("LEFT ON!\n");
+      for(int i=0; i<LEDS/2; i++){
+        strip.setPixelColor(i, orange);
+      }
+    } else {
+      Serial.print("LEFT OFF!\n");
+      for(int i=0; i<LEDS/2; i++){
+        strip.setPixelColor(i, off);
+      }
+    }
+  } else if(strip.getPixelColor(LEDS/2-1) == orange){
+    for(int i=0; i<LEDS/2; i++){
+      strip.setPixelColor(i, off);
+    }
+  }
+}
+
+void right_turn(){
+  if(right){
+    // flash turn signal
+    if(flash % 500 < 250){
+      Serial.print("RIGHT ON!\n");
+      for(int i=LEDS/2; i<LEDS; i++){
+        strip.setPixelColor(i, orange);
+      }   
+    } else {
+      Serial.print("RIGHT OFF!\n");
+      for(int i=LEDS/2; i<LEDS; i++){
+        strip.setPixelColor(i, off);
+      }
+    }
+  } else if(strip.getPixelColor(LEDS-1) == orange){
+    for(int i=LEDS/2; i<LEDS; i++){
+      strip.setPixelColor(i, off);
+    }
+  }
+}
+
+
 void loop() {
   if(digitalRead(LEFT) == LOW && millis() - time > debounce){
     left = !left;
+    time = millis();
   }
   if(digitalRead(RIGHT) == LOW && millis() - time > debounce){
     right = !right;
+    time = millis();
   }
   left_turn();
   right_turn();
@@ -57,44 +102,5 @@ void loop() {
   }
   
   strip.show(); // only call to strip.show(), updates all the leds at the same time
-  time = millis();
+  flash = millis();
 }
-
-void left_turn(){
-  if(left){
-    // flash turn signal
-    if(millis() % 1000 < 500){
-      for(int i=0; i<LEDS/2; i++){
-        strip.setPixelColor(i, orange);
-      }   
-    } else {
-      for(int i=0; i<LEDS/2; i++){
-        strip.setPixelColor(i, off);
-      }
-    }
-  } else if(strip.getPixelColor(0) == orange){
-    for(int i=0; i<LEDS/2; i++){
-      strip.setPixelColor(i, off);
-    }
-  }
-}
-
-void right_turn(){
-  if(right){
-    // flash turn signal
-    if(millis() % 1000 < 500){
-      for(int i=LEDS/2; i<LEDS; i++){
-        strip.setPixelColor(i, orange);
-      }   
-    } else {
-      for(int i=LEDS/2; i<LEDS; i++){
-        strip.setPixelColor(i, off);
-      }
-    }
-  } else if(strip.getPixelColor(0) == orange){
-    for(int i=LEDS/2; i<LEDS; i++){
-      strip.setPixelColor(i, off);
-    }
-  }
-}
-
